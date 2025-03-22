301bcde46ee3a2bea07adf75ccb73ba2e0d80611
netr <programmatical@gmail.com>
2025-03-22 14:00:10 -0700

    feat(time-entry): add controller support in manual mode
    - Move manual mode logic back into time-entry-form
    - Add save button to handle manual submission

--- resources/js/pages/time-entries/_TimeEntryForm/time-entry-form.tsx
+++ resources/js/pages/time-entries/_TimeEntryForm/time-entry-form.tsx
@@ -1,12 +1,14 @@
 import { useForm } from '@inertiajs/react';
+import { format, parse } from 'date-fns';
 import { Clock, Timer } from 'lucide-react';
-import { useRef, useState } from 'react';
+import { FormEventHandler, useRef, useState } from 'react';
 
 import InputError from '@/components/input-error';
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
-import { TimeEntryManualMode } from '@/pages/time-entries/_TimeEntryForm/time-entry-manual-mode';
+import { cn } from '@/lib/utils';
 import { TimeEntryTimerMode } from '@/pages/time-entries/_TimeEntryForm/time-entry-timer-mode';
 
 const categories = ['Development', 'Design', 'Meeting', 'Research', 'Documentation', 'Other'];
@@ -17,70 +19,118 @@ export const TimeEntryForm = () => {
     const taskNameInput = useRef<HTMLInputElement>(null);
 
     const { data, setData, errors, post, reset, processing, recentlySuccessful } = useForm({
-        taskName: '',
-        category: '',
-        startTime: '',
-        endTime: '',
+        task_title: '',
+        category_id: '',
+        start_time: '',
+        end_time: '',
     });
 
-    const handleTimeEntrySave = (duration: string) => {
+    const submit: FormEventHandler = (e) => {
+        e.preventDefault();
         post(route('time-entries.store'), {
             preserveScroll: true,
             onSuccess: () => {
                 reset();
                 taskNameInput.current?.focus();
             },
+            onError: (errors) => {
+                console.log(errors);
+            },
         });
     };
 
     const handleModeChange = (mode: 'manual' | 'timer') => {
         setTimeEntryMode(mode);
+        reset();
     };
 
-    const canSave = data.taskName && data.category && (timeEntryMode === 'manual' || timeEntryMode === 'timer');
+    const canSave = data.task_title && data.category_id && (timeEntryMode === 'manual' || timeEntryMode === 'timer');
+    const isEndTimeInvalid = data.end_time && data.start_time && data.end_time < data.start_time;
+    const isStartTimeInvalid = data.start_time && data.end_time && data.start_time > data.end_time;
 
     return (
         <div>
-            <div className="flex items-center gap-2">
-                <Input
-                    className="flex-1"
-                    onChange={(e) => setData('taskName', e.target.value)}
-                    placeholder="Task name"
-                    ref={taskNameInput}
-                    value={data.taskName}
-                />
+            <InputError message={errors.task_title} />
+            <InputError message={errors.category_id} />
+            <InputError message={errors.start_time} />
+            <InputError message={errors.end_time} />
+            <form onSubmit={submit}>
+                <div className="flex items-center gap-2">
+                    <Input
+                        className="flex-1"
+                        onChange={(e) => setData('task_title', e.target.value)}
+                        placeholder="Task title"
+                        ref={taskNameInput}
+                        value={data.task_title}
+                    />
 
-                <Select onValueChange={(e) => setData('category', e)} value={data.category}>
-                    <SelectTrigger className="w-[130px]">
-                        <SelectValue placeholder="Category" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        {categories.map((cat) => (
-                            <SelectItem key={cat} value={cat}>
-                                {cat}
-                            </SelectItem>
-                        ))}
-                    </SelectContent>
-                </Select>
+                    <Select onValueChange={(e) => setData('category_id', e)} value={data.category_id}>
+                        <SelectTrigger className="w-[130px]">
+                            <SelectValue placeholder="Category" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            {categories.map((cat) => (
+                                <SelectItem key={cat} value={cat}>
+                                    {cat}
+                                </SelectItem>
+                            ))}
+                        </SelectContent>
+                    </Select>
 
-                {timeEntryMode === 'timer' && (
-                    <TimeEntryTimerMode mode={timeEntryMode} onModeChange={handleModeChange} onSave={handleTimeEntrySave} saveDisabled={!canSave} />
-                )}
-                {timeEntryMode === 'manual' && (
-                    <TimeEntryManualMode mode={timeEntryMode} onModeChange={handleModeChange} onSave={handleTimeEntrySave} saveDisabled={!canSave} />
-                )}
+                    {timeEntryMode === 'timer' && (
+                        <>
+                            <TimeEntryTimerMode mode={timeEntryMode} onModeChange={handleModeChange} onSave={() => {}} saveDisabled={!canSave} />
+                        </>
+                    )}
+                    {timeEntryMode === 'manual' && (
+                        <>
+                            <div className={cn('flex flex-col gap-2')}>
+                                <div className="flex w-full items-center gap-2">
+                                    <div className="flex flex-1 items-center gap-2">
+                                        <Input
+                                            className={cn('flex-1', isStartTimeInvalid && 'border-red-500 bg-red-50 text-red-500')}
+                                            onChange={(e) => {
+                                                setData('start_time', formatTimeInput(e.target.value));
+                                            }}
+                                            type="time"
+                                            value={data.start_time}
+                                        />
+                                        <span className="text-muted-foreground">to</span>
+                                        <Input
+                                            className={cn('flex-1', isEndTimeInvalid && 'border-red-500 bg-red-50 text-red-500')}
+                                            onChange={(e) => {
+                                                setData('end_time', formatTimeInput(e.target.value));
+                                            }}
+                                            type="time"
+                                            value={data.end_time}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <Button type={'submit'}>Save</Button>
+                        </>
+                    )}
 
-                <ToggleGroup onValueChange={handleModeChange} type="single" value={timeEntryMode}>
-                    <ToggleGroupItem aria-label="Manual mode" value="manual">
-                        <Clock className="h-4 w-4" />
-                    </ToggleGroupItem>
-                    <ToggleGroupItem aria-label="Timer mode" value="timer">
-                        <Timer className="h-4 w-4" />
-                    </ToggleGroupItem>
-                </ToggleGroup>
-            </div>
-            <InputError message={errors.taskName} />
-            <InputError message={errors.category} />
+                    <ToggleGroup onValueChange={handleModeChange} type="single" value={timeEntryMode}>
+                        <ToggleGroupItem aria-label="Manual mode" value="manual">
+                            <Clock className="h-4 w-4" />
+                        </ToggleGroupItem>
+                        <ToggleGroupItem aria-label="Timer mode" value="timer">
+                            <Timer className="h-4 w-4" />
+                        </ToggleGroupItem>
+                    </ToggleGroup>
+                </div>
+            </form>
         </div>
     );
+};
+
+const formatTimeInput = (value: string) => {
+    try {
+        if (!value) return '';
+        const date = parse(value, 'HH:mm', new Date());
+        return format(date, 'HH:mm');
+    } catch {
+        return value;
+    }
 };
